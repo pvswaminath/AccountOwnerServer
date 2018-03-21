0682ec07e0b1c4f79d70ee4710a42dfe2987d52b
pvswaminath <venkataswaminath@gmail.com>
2018-03-21 22:04:47 +0530

    First Commit

---
+++ AccountOwnerServer/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountOwnerServer.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private IRepositoryWrapper _repoWrapper;
+
+        public ValuesController(IRepositoryWrapper repoWrapper)
+        {
+            _repoWrapper = repoWrapper;
+        }
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var domesticAccounts = _repoWrapper.Account.FindByCondition(x => x.AccountType.Equals("Domestic"));
+            var owners = _repoWrapper.Owner.FindAll();
+
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ Repository/RepositoryWrapper.cs
@@ -0,0 +1,20 @@
+﻿using Contracts;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository
+{
+    public class RepositoryWrapper : IRepositoryWrapper
+    {
+        public IOwnerRepository Owner { get; set; }
+        public IAccountRepository Account { get; set; }
+
+        public RepositoryWrapper(RepositoryContext repositoryContext)
+        {
+            this.Owner = new OwnerRepository(repositoryContext);
+            this.Account = new AccountRepository(repositoryContext);
+        }
+    }
+}
